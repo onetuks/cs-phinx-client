7bd3d35576d22ca6e8011a881103b9032ebfca53
onetuks <onetuks@naver.com>
2025-01-04 22:31:03 +0900

    Chore: 폰트 설정

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">안녕하세요</div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: DungGeunMo, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
