1179b53b38b4d201b70153f8c79e590ce199e1b8
onetuks <onetuks@naver.com>
2025-01-06 01:41:25 +0900

    Chore: font awesome icon 라이브러리 의존

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import toast from "vue-toastification";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 createApp(App).use(router).use(toast).mount("#app");
+
+library.add(faPenToSquare);
