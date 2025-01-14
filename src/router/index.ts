5f8c350fb55c3ed86600079f603bc4c415ce96d1
onetuks <onetuks@naver.com>
2025-01-15 00:53:30 +0900

    Style: 패키지 구조 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,52 +14,47 @@ const routes = [
   {
     path: "/manager/problems",
     name: "문제 관리",
-    component: () => import("@/pages/manager/problems/ProblemsManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemsManagerPage.vue"),
   },
   {
     path: "/manager/problems/choices",
     name: "객관식",
-    component: () => import("@/pages/manager/problems/ProblemsManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemsManagerPage.vue"),
   },
   {
     path: "/manager/problems/shorts",
     name: "단답형",
-    component: () => import("@/pages/manager/problems/ProblemsManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemsManagerPage.vue"),
   },
   {
     path: "/manager/problems/descriptions",
     name: "서술형",
-    component: () => import("@/pages/manager/problems/ProblemsManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemsManagerPage.vue"),
   },
   {
     path: "/manager/problems/:problemId",
     name: "문제 상세 관리",
-    component: () =>
-      import("@/pages/manager/problems/ProblemDetailManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemDetailManagerPage.vue"),
   },
   {
     path: "/manager/problems/registration",
     name: "문제 등록",
-    component: () =>
-      import("@/pages/manager/problems/ProblemDetailManagerPage.vue"),
+    component: () => import("@/pages/manager/ProblemDetailManagerPage.vue"),
   },
   {
     path: "/manager/workbooks",
     name: "문제집 관리",
-    component: () =>
-      import("@/pages/manager/workbooks/WorkBooksManagerPage.vue"),
+    component: () => import("@/pages/manager/WorkBooksManagerPage.vue"),
   },
   {
     path: "/manager/workbooks/:workbookId",
     name: "문제집 상세 관리",
-    component: () =>
-      import("@/pages/manager/workbooks/WorkBookDetailManagerPage.vue"),
+    component: () => import("@/pages/manager/WorkBookDetailManagerPage.vue"),
   },
   {
     path: "/manager/workbooks/registration",
     name: "문제집 등록",
-    component: () =>
-      import("@/pages/manager/workbooks/WorkBookDetailManagerPage.vue"),
+    component: () => import("@/pages/manager/WorkBookDetailManagerPage.vue"),
   },
 ];
 
