ef687f6f7944921ec327311cc9dc70fad897985a
onetuks <onetuks@naver.com>
2025-01-15 00:51:47 +0900

    Feat: Page 유틸 추가

---
+++ src/apis/PageUtil.ts
@@ -0,0 +1,68 @@
+export interface Page<T> {
+  content: T[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  sort: PageSort;
+  totalElements: number;
+  totalPages: number;
+  pageable: Pageable;
+}
+
+export interface PageSort {
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: PageSort;
+  offset: number;
+  unpaged: boolean;
+  paged: boolean;
+}
+
+export const emptyPage = <T>(): Page<T> => {
+  return {
+    content: [],
+    empty: true,
+    first: true,
+    last: true,
+    number: 1,
+    numberOfElements: 0,
+    size: 5,
+    sort: {
+      empty: true,
+      unsorted: true,
+      sorted: false,
+    },
+    totalElements: 0,
+    totalPages: 0,
+    pageable: {
+      pageNumber: 0,
+      pageSize: 0,
+      sort: {
+        empty: true,
+        unsorted: true,
+        sorted: false,
+      },
+      offset: 0,
+      unpaged: true,
+      paged: false,
+    },
+  };
+};
+
+export const buildPageQuery = (page?: number, size?: number): string => {
+  let query = "";
+  query += page || size ? "?" : "";
+  query += page ? `page=${page - 1}` : "";
+  query += page && size ? "&" : "";
+  query += size ? `size=${size}` : "";
+  return query;
+};
